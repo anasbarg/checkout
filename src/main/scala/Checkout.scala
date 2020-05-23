bf5b37a527795ca5317636295dc403c1b3c0ea09
Anas Albarghouthy <anas@heavenlyx.com>
2020-05-24 00:50:52 +0300

    Refactored code

--- src/main/scala/Checkout.scala
+++ src/main/scala/Checkout.scala
@@ -1,5 +1,62 @@
-
 object Checkout extends App {
+
+  case class Product(productId: Char, unitPrice: Int, offer: Option[Offer])
+  case class Offer(numberNeededForDiscount: Int, batchPrice: Int)
+
+  def calculateNumberOfOccurances(products: List[Product]): Map[Char, Int] =
+    products
+      .groupBy(_.productId)
+      .map { case (pid, instances) => (pid, instances.length) } // 1
+      .withDefaultValue(0) // 2
+
+  /* 1. You can pass a partial function to `map`
+   * in order to destructure the tuple instead of using `tuple._1`
+   * and `tuple._2`.
+   *
+   * 2. You can use `Map#withDefault` to provide a default
+   * value when sending it a key that doesn't exist so it doesn't
+   * throw `NoSuchElementException`.
+   * E.g. `Map("One" -> 1)("Two")` will throw an exception.
+   */
+
+  def calculateCostForDiscountedItem(
+      product: Product,
+      numberOfItems: Int
+  ): Int = product.offer match { // 3
+    case None => product.unitPrice * numberOfItems
+    case Some(offer) => {
+      val excludedFromDiscount = numberOfItems % offer.numberNeededForDiscount
+      val discountBatches = numberOfItems / offer.numberNeededForDiscount
+      (excludedFromDiscount * product.unitPrice) + (discountBatches * offer.batchPrice)
+    }
+  }
+  
+
+  /* 3. Most of the time, a `match` is the cleanest way to
+   * access values within `Option`s and `Either`s..
+   * Getting used to the `map` and `flatMap` methods of
+   * these types can also be very helpful.
+   * For comprehensions are actually just syntactic sugar for
+   * `map`, `filter`, and `flatMap`. See https://docs.scala-lang.org/tour/for-comprehensions.html
+   */
+
+  def calculateTotalValueOfBasket(products: List[Product]): Int = { // 4
+    val itemOccurances: Map[Char, Int] =
+      calculateNumberOfOccurances(products)
+
+    products.distinct.map { product => // 5
+      calculateCostForDiscountedItem(product, itemOccurances(product.productId))
+    }.sum
+  }
+
+  /* 4. `calculateTotalValueOfBasket` is now a *pure function*. It used to
+   * print the results to the screen, which is a side effect.
+   * It also used to depend on the global value `basket`,
+   * which made it less reusable.
+   *
+   * 5. We used `List#distinct` method to make sure that the
+   * price of an item is not calculated multiple times.
+   */
 
   val basket = List(
     Product('A', 50, Some(Offer(3, 130))),
@@ -12,45 +69,9 @@ object Checkout extends App {
     Product('A', 50, Some(Offer(3, 130)))
   )
 
-  def calculateNumberOfOccurances(products: List[Product]): Map[Char, Int] = {
-    val listOfProducts = products.map(_.productId)
-    listOfProducts.groupBy(l => l).map(t => (t._1, t._2.length))
-  }
+  // A fun trick:
+  println(
+    s"${Console.YELLOW}Total price:${Console.GREEN} ${calculateTotalValueOfBasket(basket)}${Console.RESET}"
+  )
 
-  def calculateCostForDiscountedItem(numberOfItems: Int, individualCost: Int, offers: Option[Offer]): Int = {
-    val withoutAnyDiscounts = numberOfItems * individualCost
-
-    val withDiscount = for {
-    batchPrice <- offers.map(offer => offer.batchPrice)
-    numNeededForDiscount <- offers.map(offer => offer.numberNeededForDiscount)
-    } yield {
-      val remainder = numberOfItems % numNeededForDiscount
-      val numberIncludedInDiscount = (numberOfItems - remainder) / numNeededForDiscount
-      (numberIncludedInDiscount * batchPrice) + (remainder * individualCost)
-    }
-
-    withDiscount match {
-      case Some(value) => value
-      case None => withoutAnyDiscounts
-    }
-  }
-
-  def calculateTotalValueOfBasket(products: List[Product]): Unit = {
-    val ItemsWithOccurances: Map[Char, Int] = calculateNumberOfOccurances(basket)
-
-    val a = for {
-    priceForA <- products.map(product => calculateCostForDiscountedItem(ItemsWithOccurances('A'), product.unitPrice, product.offer))
-    priceForB <- products.map(product => calculateCostForDiscountedItem(ItemsWithOccurances('B'), product.unitPrice, product.offer))
-    priceForC <- products.map(product => calculateCostForDiscountedItem(ItemsWithOccurances('C'), product.unitPrice, product.offer))
-    } yield priceForB
-
-    println(a)
-
-  }
-
-  calculateTotalValueOfBasket(basket)
-
-
-  case class Product(productId: Char, unitPrice: Int, offer: Option[Offer])
-  case class Offer(numberNeededForDiscount: Int, batchPrice: Int)
 }
